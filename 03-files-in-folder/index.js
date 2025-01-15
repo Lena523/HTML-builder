aad7bb0876ea15dde17a3a544632166cdec3fef4
Elena Khadasevich <camatsu90@mail.ru>
2025-01-15 21:55:08 +0300

    feat: add Files in Folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,1 +1,18 @@
-
+const path = require('path');
+const fs = require("fs").promises;
+fs.readdir(path.join(__dirname, 'secret-folder'),{
+withFileTypes:true
+}).then(files =>{
+  files.forEach(file =>{
+   if(!file.isDirectory()){
+    const filePath = path.join(__dirname, 'secret-folder', file.name);
+    const fileName = path.basename(filePath);
+    const ext = path.extname(filePath);
+    fs
+    .stat(filePath)
+    .then(res =>{
+     console.log(`${fileName.replace(ext, '')} - ${ext.replace('.', '')} - ${Number(res.size)}b`);
+    });
+   }
+  });
+});
