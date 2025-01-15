bd9b3459d12fb9ec19520bd73592ee38af67a105
Elena Khadasevich <camatsu90@mail.ru>
2025-01-15 17:30:37 +0300

    feat : add Writing Console Input to File

---
+++ 02-write-file/index.js
@@ -0,0 +1,20 @@
+const fs = require('fs'); 
+const path = require('path'); 
+const textPath = path.join(__dirname, 'text.txt');
+const output = fs.createWriteStream(textPath);
+process.stdout.write('Hi!, please write something...\n');
+process.stdin.on('data', data =>{
+ if(data.toString().trim() === 'exit'){
+  closeFile();
+ }
+ output.write(data);
+});
+
+process.on('SIGINT', closeFile);
+
+function closeFile(){
+  process.stdout.write('\nSee you next time!');
+  process.exit();
+}
+
+
