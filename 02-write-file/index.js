45a43dbd273eb3ba1bb8137035e909b9e35e9401
Elena Khadasevich <camatsu90@mail.ru>
2025-01-19 00:50:41 +0300

    fix: fix 02 and 04 tasks

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path'); 
 const textPath = path.join(__dirname, 'text.txt');
 const output = fs.createWriteStream(textPath);
-process.stdout.write('Hi! Please, write something...\n');
+process.stdout.write('Hi! Please write something...\n');
 process.stdin.on('data', data =>{
  if(data.toString().trim() === 'exit'){
   closeFile();
